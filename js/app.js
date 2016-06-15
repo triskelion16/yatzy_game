e1837c8827c31c7ac1526ea7fbb8f8fe30d1c0cf
triskelion16 <triskelion.work@gmail.com>
2016-06-15 12:58:05 +0200

    layout- complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+    
+    var roll = $('.main_button').find('button');
+    roll.on('click', function() {
+        randomDice();
+    });
  
+    
+    
 });
+
+function randomDice() {
+    var box = $('.main_dice').find('div');
+    var showDice = 'showDice';
+    
+    box.addClass(showDice + randomNumber());
+    
+    console.log(showDice + randomNumber());
+}
+
+function randomNumber() {
+    var numberOfDice = Math.ceil(Math.random() * 6);
+    return numberOfDice;
+}
