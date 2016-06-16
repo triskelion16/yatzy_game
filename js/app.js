1e87d23975479862df06ba5e77e1de929c7625d7
triskelion16 <triskelion.work@gmail.com>
2016-06-16 11:39:56 +0200

    add: js- innerHtml 1-6 elements

--- js/app.js
+++ js/app.js
@@ -1,49 +1,124 @@
 $(document).ready(function() {
-    var dice_points = [];
+    
+    roll();
+ 
+    shadow_class_add();
+    
+});
+
+/***** main roll *****/
+function roll() {
+    var dice_points_table = [];
     var temp_table = [];
-    /*var dice1_points;
-    var dice2_points; 
-    var dice3_points; 
-    var dice4_points; 
-    var dice5_points; */
     
     var roll_button = $('.main_button').find('button');
     roll_button.on('click', function() {
         
         for(var i=1; i<=5; i++) {
-            dice_points[i] = randomDice(i);
+            dice_points_table[i] = randomDice(i);
             
-            if(dice_points[i] === 0) {
-               dice_points[i] = temp_table[i];
+            if(dice_points_table[i] === 0) {
+               dice_points_table[i] = temp_table[i];
             }
-            
         }
-            console.log(dice_points);
-            console.log(temp_table);
         
-        /*dice1_points = randomDice(1);
-        dice2_points = randomDice(2);
-        dice3_points = randomDice(3);
-        dice4_points = randomDice(4);
-        dice5_points = randomDice(5);*/
-        temp_table = dice_points.slice(0);
-    });
- 
-    var dice_all = $('.main_dice');
-    shadow_class_add(dice_all);
-    
-    //console.log(dice1_points + dice2_points + dice3_points + dice4_points + dice5_points);
-});
-
-function shadow_class_add(dice_all) {
-    var dice = dice_all.find('div');
-    
-    dice.on('click', function() {
-        $(this).toggleClass('shadow_dice');
-        console.log($(this));
+        temp_table = dice_points_table.slice(0);
+        //console.log(dice_points_table);
+        
+        /*** if-s 1-6***/
+        var points_ones = ones(dice_points_table);
+        var points_twos = twos(dice_points_table);
+        var points_threes = threes(dice_points_table);
+        var points_fours = fours(dice_points_table);
+        var points_fives = fives(dice_points_table);
+        var points_sixs = sixs(dice_points_table);
+        
+        console.log(points_ones+points_twos+points_threes+points_fours+points_fives+points_sixs);
     });
 }
 
+/***** if-s 1-6*****/
+
+function ones(points_table) {
+    var sum = 0;
+    var one_element = $('#one').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 1) {
+            sum += 1;
+        }    
+    }
+    one_element.html(sum);
+    return sum;
+}
+
+function twos(points_table) {
+    var sum = 0;
+    var two_element = $('#two').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 2) {
+            sum += 2;
+        }    
+        two_element.html(sum);
+    }
+    return sum;
+}
+
+function threes(points_table) {
+    var sum = 0;
+    var three_element = $('#three').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 3) {
+            sum += 3;
+        }    
+        three_element.html(sum);
+    }
+    return sum;
+}
+
+function fours(points_table) {
+    var sum = 0;
+    var four_element = $('#four').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 4) {
+            sum += 4;
+        }    
+        four_element.html(sum);
+    }
+    return sum;
+}
+
+function fives(points_table) {
+    var sum = 0;
+    var five_element = $('#five').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 5) {
+            sum += 5;
+        }    
+        five_element.html(sum);
+    }
+    return sum;
+}
+
+function sixs(points_table) {
+    var sum = 0;
+    var six_element = $('#six').find('span');
+    
+    for(var i=1; i<points_table.length; i++) {
+        if(points_table[i] === 6) {
+            sum += 6;
+        }    
+        six_element.html(sum);
+    }
+    return sum;
+}
+
+
+/***** random dice *****/
 function randomDice(id) {
     var dice = $('#dice' + id);
     
@@ -61,4 +136,14 @@ function randomDice(id) {
 function randomNumber() {
     var numberOfDice = Math.ceil(Math.random() * 6);
     return numberOfDice;
+}
+
+/***** add shadow class *****/
+function shadow_class_add(dice_all) {
+    var dice = $('.main_dice').find('div');
+    
+    dice.on('click', function() {
+        $(this).toggleClass('shadow_dice');
+        console.log($(this));
+    });
 }
